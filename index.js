e7d8917936afa08712eff6e63310aafb83f039eb
iannl <66180087+iannl@users.noreply.github.com>
2025-07-22 17:41:09 -0700

    Update copyright year

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//   Copyright 2024 iannl
+//   Copyright 2025 iannl
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
